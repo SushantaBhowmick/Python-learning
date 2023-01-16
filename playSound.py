a9d4d8b566800d92750add14164502689480c01a
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-01-16 22:22:57 +0530

    new thing

--- playSound.py
+++ playSound.py
@@ -1,6 +1,20 @@
 # from playsound import playsound
 # playsound('E:\\Python\\play.mp3')
+#
+# import os
+# print(os.listdir())
 
-import os
+a= "harry"
+b=345
+c=25.2
 
-print(os.listdir())
+person = {
+    "firstName": "John",
+    "lastName": "Doe",
+    "age": 28
+}
+d=type(person)
+print(d)
+
+print(a,b,c)
+
